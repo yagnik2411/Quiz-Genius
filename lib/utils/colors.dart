fc3eeaefca835e6805ebecf2060f1667ccd4e24b
yagnik panchal <panchalyagnik2411@gmail.com>
2023-06-07 12:10:40 +0530

    colors Added

---
+++ lib/utils/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+  static const Color lime = Color(0xFFC4DFDF);
+  static const Color seashall = Color(0xFFF8F6F4);
+  static const Color lightLime = Color(0xFFE3F4F4);
+  static const Color midLime = Color(0xFFD2E9E9);
+  static const Color mint = Color(0xFF8EC3B0);
+  static const Color elfGreen = Color(0xFF9ED5C5);
+  static const Color seaGreen = Color(0xFFBCEAD5);
+  static const Color lightCyan = Color(0xFFDEF5E5);
+  
+
+}
