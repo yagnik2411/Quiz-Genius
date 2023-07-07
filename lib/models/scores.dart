8c69b0f3f2b17a74ba888ab4234b847aa0f9bdc7
yagnik panchal <panchalyagnik2411@gmail.com>
2023-07-07 16:00:51 +0530

    camelcase improve

--- lib/models/scores.dart
+++ lib/models/scores.dart
@@ -1,17 +1,17 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
 class Scores {
-  static List<score> scores = [score(correct: 7, scoreInPercent: 70,date: DateTime.utc(2023,06,15))];
+  static List<Score> scores = [Score(correct: 7, scoreInPercent: 70,date: DateTime.utc(2023,06,15))];
   // Scores({
   //   required this.scores,
   // });
 }
 
-class score {
+class Score {
   final int correct;
   final int scoreInPercent;
   final DateTime date;
-  score({
+  Score({
     required this.correct,
     required this.scoreInPercent,
     required this.date,
