3e697060a5da1c475794ddd66a386710b4af2e55
yagnik panchal <panchalyagnik2411@gmail.com>
2023-07-08 12:37:54 +0530

    local commit

---
+++ lib/models/current_user.dart
@@ -0,0 +1,49 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class CurretUser {
+  static late UserName currentUser;
+}
+
+class UserName {
+  final String email;
+  final String password;
+  String userName;
+  int performance;
+
+  UserName(
+      {required this.email,
+      required this.password,
+      this.userName = "",
+      this.performance = 100});
+
+  String setUserName(String userName) => this.userName = userName;
+
+  performanceUpadate(
+      {required BuildContext context,
+      required String currentEmail,
+      required int currentPerformance}) async {
+    DocumentReference firestore =
+        FirebaseFirestore.instance.collection("users").doc(currentEmail);
+    await firestore.update({
+      'performance': currentPerformance,
+    });
+    performance = currentPerformance;
+  }
+
+  void add({required BuildContext context}) async {
+    Map<String, dynamic> data = {
+      "email": email,
+      "password": password,
+      "userName": userName,
+      "performance": performance,
+    };
+    DocumentReference firestore =
+        FirebaseFirestore.instance.collection("users").doc("${data["email"]}");
+
+    await firestore
+        .set(data)
+        .whenComplete(() => print("data added"))
+        .onError((error, stackTrace) => print(error));
+  }
+}
