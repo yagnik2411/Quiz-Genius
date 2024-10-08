e042b3d99361707a9e37422061d9a3883c4d3368
lavitratyagi1 <lavitratyagi@gmail.com>
2024-10-08 14:21:32 +0530

    Fixed submit button

--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -24,13 +24,13 @@ class _QuizPageState extends State<QuizPage> {
   List<bool> isAdd = List.generate(10, (i) => false);
   List<int> isCorrect = List.generate(10, (i) => -1);
   int correct = 0;
-  // List<PreviousQuestions> previousQuestions = [];
+  bool isSubmitButtonDisabled = false; // Prevents multiple submissions
+
   @override
   void initState() {
     super.initState();
-    // Fetch quiz questions and clear any old quiz data
     quizFuture = Questions().getQuestions();
-    PreviousQuestions.questions.clear();  // Clear previous questions when new quiz starts
+    PreviousQuestions.questions.clear(); // Clear previous questions for a new quiz
   }
 
   @override
@@ -62,134 +62,135 @@ class _QuizPageState extends State<QuizPage> {
             final quiz = snapshot.data!;
 
             return ListView.builder(
-                padding:
-                    EdgeInsets.symmetric(vertical: 8.sp, horizontal: 16.sp),
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return Container(
-                    padding: EdgeInsets.all(8.sp),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: MyColors.darkCyan, width: 3),
-                      borderRadius: BorderRadius.circular(15.sp),
-                      color: MyColors.malachite.withOpacity(0.8),
-                    ),
-                    child: Column(
-                      children: [
-                        ListTile(
-                          title: Text(
-                            quiz[index].question,
-                            style: TextStyle(
-                              color: MyColors.seashall,
-                              fontSize: 15.sp,
-                              fontWeight: FontWeight.values[5],
-                            ),
-                            textWidthBasis: TextWidthBasis.parent,
+              padding: EdgeInsets.symmetric(vertical: 8.sp, horizontal: 16.sp),
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Container(
+                  padding: EdgeInsets.all(8.sp),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: MyColors.darkCyan, width: 3),
+                    borderRadius: BorderRadius.circular(15.sp),
+                    color: MyColors.malachite.withOpacity(0.8),
+                  ),
+                  child: Column(
+                    children: [
+                      ListTile(
+                        title: Text(
+                          quiz[index].question,
+                          style: TextStyle(
+                            color: MyColors.seashall,
+                            fontSize: 15.sp,
+                            fontWeight: FontWeight.values[5],
                           ),
+                          textWidthBasis: TextWidthBasis.parent,
                         ),
-                        ButtonBar(
-                          alignment: MainAxisAlignment.spaceBetween,
-                          buttonPadding: EdgeInsets.symmetric(
-                              horizontal: 20.sp, vertical: 10.sp),
-                          children: [
-                            ElevatedButton(
-                              onPressed: () {
-                                if (isAdd[index] == false) {
-                                  setState(() {
-                                    isAdd[index] = true;
+                      ),
+                      ButtonBar(
+                        alignment: MainAxisAlignment.spaceBetween,
+                        buttonPadding: EdgeInsets.symmetric(
+                          horizontal: 20.sp,
+                          vertical: 10.sp,
+                        ),
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              if (!isAdd[index]) {
+                                setState(() {
+                                  isAdd[index] = true;
+                                  if (quiz[index].answer == true) {
+                                    isCorrect[index] = 0;
+                                    toMassage(msg: "correct");
+                                    correct++;
+                                  } else {
+                                    isCorrect[index] = 1;
+                                    toMassage(msg: "incorrect");
+                                  }
 
-                                    if (quiz[index].answer == true) {
-                                      isCorrect[index] = 0;
-                                      toMassage(msg: "correct");
-                                      correct++;
-                                    } else {
-                                      isCorrect[index] = 1;
-                                      toMassage(msg: "incorrect");
-                                    }
-
-                                    // Add the question to PreviousQuestions
-                                    PreviousQuestions.questions.add(
-                                        PreviousQuestion(
-                                            id: index,
-                                            question: quiz[index].question,
-                                            correct: isCorrect[index] == 0
-                                                ? true
-                                                : false));
-                                  });
-                                }
-                              },
-                              style: ButtonStyle(
-                                backgroundColor: (isAdd[index] == false)
-                                    ? MaterialStateProperty.all(MyColors.mint)
-                                    : ((isCorrect[index] == 0)
-                                        ? MaterialStateProperty.all(
-                                            Colors.green)
-                                        : MaterialStateProperty.all(
-                                            Colors.red)),
-                                elevation: MaterialStateProperty.all(10),
-                                fixedSize: MaterialStateProperty.all(
-                                    Size(120.w, 40.h)),
-                                side: MaterialStateProperty.all(
-                                    const BorderSide(color: Colors.white)),
-                                shape: MaterialStateProperty.all(
-                                  RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(15.sp),
-                                  ),
+                                  // Add question to PreviousQuestions
+                                  PreviousQuestions.questions.add(
+                                    PreviousQuestion(
+                                      id: index,
+                                      question: quiz[index].question,
+                                      correct: isCorrect[index] == 0,
+                                    ),
+                                  );
+                                });
+                              }
+                            },
+                            style: ButtonStyle(
+                              backgroundColor: (isAdd[index] == false)
+                                  ? MaterialStateProperty.all(MyColors.mint)
+                                  : ((isCorrect[index] == 0)
+                                      ? MaterialStateProperty.all(Colors.green)
+                                      : MaterialStateProperty.all(Colors.red)),
+                              elevation: MaterialStateProperty.all(10),
+                              fixedSize: MaterialStateProperty.all(
+                                Size(120.w, 40.h),
+                              ),
+                              side: MaterialStateProperty.all(
+                                const BorderSide(color: Colors.white),
+                              ),
+                              shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15.sp),
                                 ),
                               ),
-                              child: "True".text.xl.make(),
                             ),
-                            ElevatedButton(
-                              onPressed: () {
-                                if (isAdd[index] == false) {
-                                  setState(() {
-                                    isAdd[index] = true;
-                                    if (quiz[index].answer == false) {
-                                      isCorrect[index] = 1;
-                                      toMassage(msg: "correct");
-                                      correct++;
-                                    } else {
-                                      isCorrect[index] = 0;
-                                      toMassage(msg: "incorrect");
-                                    }
+                            child: "True".text.xl.make(),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              if (!isAdd[index]) {
+                                setState(() {
+                                  isAdd[index] = true;
+                                  if (quiz[index].answer == false) {
+                                    isCorrect[index] = 1;
+                                    toMassage(msg: "correct");
+                                    correct++;
+                                  } else {
+                                    isCorrect[index] = 0;
+                                    toMassage(msg: "incorrect");
+                                  }
 
-                                    // Add the question to PreviousQuestions
-                                    PreviousQuestions.questions.add(
-                                        PreviousQuestion(
-                                            id: index,
-                                            question: quiz[index].question,
-                                            correct: isCorrect[index] == 1
-                                                ? true
-                                                : false));
-                                  });
-                                }
-                              },
-                              style: ButtonStyle(
-                                backgroundColor: (isAdd[index] == false)
-                                    ? MaterialStateProperty.all(MyColors.mint)
-                                    : ((isCorrect[index] == 1)
-                                        ? MaterialStateProperty.all(
-                                            Colors.green)
-                                        : MaterialStateProperty.all(
-                                            Colors.red)),
-                                elevation: MaterialStateProperty.all(10),
-                                fixedSize: MaterialStateProperty.all(
-                                    Size(120.w, 40.h)),
-                                side: MaterialStateProperty.all(
-                                    const BorderSide(color: Colors.white)),
-                                shape: MaterialStateProperty.all(
-                                  RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(15.sp),
-                                  ),
+                                  // Add question to PreviousQuestions
+                                  PreviousQuestions.questions.add(
+                                    PreviousQuestion(
+                                      id: index,
+                                      question: quiz[index].question,
+                                      correct: isCorrect[index] == 1,
+                                    ),
+                                  );
+                                });
+                              }
+                            },
+                            style: ButtonStyle(
+                              backgroundColor: (isAdd[index] == false)
+                                  ? MaterialStateProperty.all(MyColors.mint)
+                                  : ((isCorrect[index] == 1)
+                                      ? MaterialStateProperty.all(Colors.green)
+                                      : MaterialStateProperty.all(Colors.red)),
+                              elevation: MaterialStateProperty.all(10),
+                              fixedSize: MaterialStateProperty.all(
+                                Size(120.w, 40.h),
+                              ),
+                              side: MaterialStateProperty.all(
+                                const BorderSide(color: Colors.white),
+                              ),
+                              shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15.sp),
                                 ),
                               ),
-                              child: "False".text.xl.make(),
                             ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ).py(5.sp);
-                });
+                            child: "False".text.xl.make(),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ).py(5.sp);
+              },
+            );
           }
         },
       ),
@@ -209,38 +210,58 @@ class _QuizPageState extends State<QuizPage> {
         child: Container(
           margin: EdgeInsets.symmetric(horizontal: 15.sp, vertical: 5.sp),
           child: ElevatedButton(
-            onPressed: () async {
-              await scoreUpdate(context);
-              int currentPerformance =
-                  ((correct * 10) + CurrentUser.currentUser.performance) ~/ 2;
-              CurrentUser.currentUser.performanceUpdate(
-                  context: context,
-                  currentEmail: CurrentUser.currentUser.email,
-                  currentPerformance: currentPerformance);
+            onPressed: isSubmitButtonDisabled
+                ? null
+                : () async {
+                    if (isAdd.contains(false)) {
+                      toMassage(msg: "Please answer all questions!");
+                      return;
+                    }
 
-              Scores.updateScores(
-                  context: context,
-                  score: Scores.scores,
-                  email: CurrentUser.currentUser.email);
-              
-              // Update Firestore with the new list of previous questions
-              PreviousQuestions.addToCollection(
-                  context: context,
-                  question: PreviousQuestions.questions,
-                  email: CurrentUser.currentUser.email);
-              
-              Navigator.pushReplacementNamed(context, MyRoutes.homeRoute);
-            },
+                    setState(() {
+                      isSubmitButtonDisabled = true; // Disable submit button
+                    });
+
+                    await scoreUpdate(context);
+                    int currentPerformance = ((correct * 10) +
+                            CurrentUser.currentUser.performance) ~/
+                        2;
+
+                    CurrentUser.currentUser.performanceUpdate(
+                      context: context,
+                      currentEmail: CurrentUser.currentUser.email,
+                      currentPerformance: currentPerformance,
+                    );
+
+                    Scores.updateScores(
+                      context: context,
+                      score: Scores.scores,
+                      email: CurrentUser.currentUser.email,
+                    );
+
+                    // Add previous questions to Firestore
+                    PreviousQuestions.addToCollection(
+                      context: context,
+                      question: PreviousQuestions.questions,
+                      email: CurrentUser.currentUser.email,
+                    );
+
+                    Navigator.pushReplacementNamed(
+                      context,
+                      MyRoutes.homeRoute,
+                    );
+                  },
             style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(MyColors.mint),
+              backgroundColor: isSubmitButtonDisabled
+                  ? MaterialStateProperty.all(Colors.grey)
+                  : MaterialStateProperty.all(MyColors.mint),
               elevation: MaterialStateProperty.all(10),
               side: MaterialStateProperty.all(
-                  const BorderSide(color: MyColors.seashall, width: 2)),
+                const BorderSide(color: MyColors.seashall, width: 2),
+              ),
               shape: MaterialStateProperty.all(
                 RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(
-                    15.sp,
-                  ),
+                  borderRadius: BorderRadius.circular(15.sp),
                 ),
               ),
             ),
@@ -252,19 +273,19 @@ class _QuizPageState extends State<QuizPage> {
   }
 
   void scoreListAdd(BuildContext context) {
-    // Add the new score to the list
+    // Add new score
     Scores.scores.add(Score(
       correct: correct,
       scoreInPercent: correct * 10,
       date: DateFormat('dd / MM / yyyy').format(DateTime.now()).toString(),
     ));
 
-    // Ensure we only keep the last 10 scores
+    // Keep only the last 10 scores
     if (Scores.scores.length > 10) {
       Scores.scores = Scores.scores.sublist(Scores.scores.length - 10);
     }
 
-    // Update the scores in Firestore
+    // Update Firestore with the new scores
     Scores.addScores(
       context: context,
       score: Scores.scores,
@@ -273,7 +294,6 @@ class _QuizPageState extends State<QuizPage> {
   }
 
   Future<void> scoreFetch() async {
-    print("score: ${CurrentUser.currentUser.email}");
     DocumentReference userDocRef = FirebaseFirestore.instance
         .collection("users")
         .doc(CurrentUser.currentUser.email)
@@ -284,10 +304,7 @@ class _QuizPageState extends State<QuizPage> {
       DocumentSnapshot data = await userDocRef.get();
 
       if (data.exists) {
-        // Document with scores exists, fetch scores
         List temp = data['scores'];
-        print(temp.length);
-
         for (int i = 0; i < temp.length; i++) {
           Scores.scores.add(Score(
             correct: data['scores'][i]['correct'],
@@ -295,10 +312,7 @@ class _QuizPageState extends State<QuizPage> {
             date: data['scores'][i]['date'],
           ));
         }
-
-        print(Scores.scores.length);
       } else {
-        // Document does not exist, create a new one with an empty list
         await userDocRef.set({'scores': []});
       }
     } catch (e) {
@@ -307,13 +321,8 @@ class _QuizPageState extends State<QuizPage> {
   }
 
   Future<void> scoreUpdate(BuildContext context) async {
-    // Clear the current scores
     Scores.scores.clear();
-
-    // Fetch existing scores
     await scoreFetch();
-
-    // Add the new score and ensure the list has only the last 10 scores
     scoreListAdd(context);
   }
 }
